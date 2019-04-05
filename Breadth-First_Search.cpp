066a8eac0f46b151e271c27b2925d2925ecdb558
pibi9265 <pibi9265@outlook.com>
2019-04-05 15:39:37 +0900

    201904051539

--- Breadth-First_Search.cpp
+++ Breadth-First_Search.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <utility> //for pair class
+#include <string> //for string
 
-int main(){
-    return 0;
+#include "Breadth-First_Search.h"
+
+using namespace std;
+
+bfSearch::bfSearch(){
+    i = 0;
 }
---
+++ Breadth-First_Search.h
@@ -0,0 +1,11 @@
+#ifndef bfSearch_H
+#define bfSearch_H
+class bfSearch{
+    private:
+        int i;
+    public:
+        bfSearch();
+        ~bfSearch();
+        serching();
+};
+#endif
