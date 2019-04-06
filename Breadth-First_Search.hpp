bf356ad5938368517bc2a32edbaa31c3d97d4e72
pibi9265 <pibi9265@outlook.com>
2019-04-06 22:16:10 +0900

    201904062216

--- Breadth-First_Search.hpp
+++ Breadth-First_Search.hpp
@@ -8,41 +8,61 @@ using namespace std;
 
 class bfSearch{
     private:
-        int bfArrX;
-        int bfArrY;
+        int bfArrSize;
         int **bfArr;
         int start;
         int end;
-        queue<int> open;
-        stack<int> closed;
+        queue<queue<int>> open;
+        int *closed;
     public:
-        bfSearch(int bfArrX, int bfArrY, int **bfArr, int start, int end);
+        bfSearch(int bfArrSize, int **bfArr, int start, int end);
         ~bfSearch();
         string searching();
 };
 
-bfSearch::bfSearch(int bfArrX, int bfArrY, int **bfArr, int start, int end){
-    this->bfArrX = bfArrX;
-    this->bfArrY = bfArrY;
+bfSearch::bfSearch(int bfArrSize, int **bfArr, int start, int end){
+    this->bfArrSize = bfArrSize;
     this->bfArr = bfArr;
     this->start = start;
     this->end = end;
+    closed = new int[bfArrSize];
+    for(int i = 0;i < bfArrSize;i++){
+        closed[i] = 0;
+    }
 }
 
-bfSearch::~bfSearch(){}
+bfSearch::~bfSearch(){
+    delete[] closed;
+}
 
 string bfSearch::searching(){
-    string result = to_string(start);
-    result += "->";
-    while(!open.empty()){
-        open.pop();
+    string result;
+    open.push(queue<int>());
+    open.front().push(start);
+    while(true){
+        if(open.front().back()==end){
+            while(true){
+                result += to_string(open.front().front());
+                open.front().pop();
+                if(open.front().empty()){
+                    break;
+                }else{
+                    result += "->";
+                }
+            }
+            return result;
+        }
+        else{
+            for(int i = 0;i < bfArrSize;i++){
+                if(bfArr[open.front().back()][i]!=0&&closed[i]==0){
+                    open.push(open.front());
+                    open.back().push(i);
+                }
+            }
+            closed[open.front().back()] = 1;
+            open.pop();
+        }
     }
-    while(!closed.empty()){
-        result += to_string(closed.top());
-        result += "->";
-        closed.pop();
-    }
-    return result;
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -5,18 +5,39 @@ using namespace std;
 #include "Breadth-First_Search.hpp"
 
 int main(){
-    int bfArrX = 20;
-    int bfArrY = 20;
+    int bfArrSize = 5;
 
-    int start = 17;
+    int **bfArr = new int *[bfArrSize];
+    for(int i = 0;i < bfArrSize;i++){
+        bfArr[i] = new int[bfArrSize];
+    }
+    for(int y = 0; y < bfArrSize;y++){
+        for(int x = 0;x < bfArrSize;x++){
+            bfArr[y][x] = 0;
+        }
+    }
+
+    bfArr[0][1] = 1;
+    bfArr[0][3] = 1;
+    bfArr[0][4] = 1;
+    bfArr[1][0] = 1;
+    bfArr[2][3] = 1;
+    bfArr[3][0] = 1;
+    bfArr[3][2] = 1;
+    bfArr[4][0] = 1;
+
+    int start = 0;
     int end = 2;
 
-    //bfSearch(bfArrX, bfArrY, **bfArr, start, end);
+    bfSearch *bf = new bfSearch(bfArrSize, bfArr, start, end);
+    cout << bf->searching() << endl;
 
-    string str = "Hello";
-    str = str + " world!! ";
-    str = str + to_string(100);
-    cout << str << endl;
+    for(int i = 0;i < bfArrSize;i++){
+        delete[] bfArr[i];
+    }
+    delete[] bfArr;
+
+    delete bf;
 
     return 0;
 }
